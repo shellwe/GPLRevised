280db46945748c9c3ecb66f9d1a7c5a5501ac676
Shawn Hellwege <shellwe@gmail.com>
2016-10-30 22:24:30 -0500

    Added a responsive class for images
    Going through areas where I embedded the responsive code and deleting
    it.

--- header.php
+++ header.php
@@ -14,21 +14,31 @@
 <body>
 	<div id="capsule">
 		<header id="header" class="container">
-			<div class="logo"> <img src="images/logo.png" alt="Great Plains Landscaping Logo" /> </div>
+			<div class="logo responsive">
+				<img src="images/logo.png" alt="Great Plains Landscaping Logo"/>
+			</div>
 			<div id="header-icons-mobile">
 				<a class="telephone" href="tel:402-417-6906"><i class="fa fa-phone" aria-hidden="true"></i></a>
 				<i class="fa fa-bars" aria-hidden="true"></i>
 			</div>
 			<nav class="main-navigation toggle-menu" role="navigation">
 				<ul>
-					<li><a href="#">Home</a></li>
-					<li><a href="#">About</a></li>
-					<li><a href="#">Design</a></li>
-					<li><a href="#">Galleries</a></li>
-					<li><a href="#">Services</a></li>
-					<li><a href="#">Advice</a></li>
-					<li><a href="testimonials.php">Testimonials</a></li>
-					<li><a href="#">Contact</a></li>
+					<li><a href="#">Home</a>
+					</li>
+					<li><a href="#">About</a>
+					</li>
+					<li><a href="#">Design</a>
+					</li>
+					<li><a href="#">Galleries</a>
+					</li>
+					<li><a href="#">Services</a>
+					</li>
+					<li><a href="#">Advice</a>
+					</li>
+					<li><a href="testimonials.php">Testimonials</a>
+					</li>
+					<li><a href="#">Contact</a>
+					</li>
 				</ul>
 			</nav>
 		</header>
